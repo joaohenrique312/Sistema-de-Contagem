0d84532a423be67ed0a6d8e993a80f29cc957469
João Henrique <112218661+joaohenrique312@users.noreply.github.com>
2023-02-17 11:18:02 -0400

    Condições melhores para o sistema
    O sistema tinha um defeito de detectar objetos com as keys undefined, então criei uma espécie de detector que vê até quantos produtos tem em cada objeto...

--- verificar.js
+++ verificar.js
@@ -17,15 +17,53 @@ function verificar(){
                 if(urlGeral[code.value].QTDE > 0){
                     info.innerHTML = `<p>Segue as informações do produto ${urlGeral[code.value].PRODUTO} abaixo:</p>
                     <p><strong>Alerta!</strong> O formato de data está em: mês/dia/ano!
-                    <p>Ao primeiro lote, existem ${urlGeral[code.value].QTDE} até o dia ${urlGeral[code.value].VENC1}.</p>
-                    <p>Ao segundo lote, existem ${urlGeral[code.value].QTDE2} até o dia ${urlGeral[code.value].VENC2}. </p>
-                    <p>Ao terceiro lote, existem ${urlGeral[code.value].QTDE3} até o dia ${urlGeral[code.value].VENC3}. </p>
-                    <p>Ao quarto lote, existem ${urlGeral[code.value].QTDE4} até o dia ${urlGeral[code.value].VENC4}. </p>
-                    <p>Ao quinto lote, existem ${urlGeral[code.value].QTDE5} até o dia ${urlGeral[code.value].VENC5}. </p>
-                    <p>Totalizando um total de ${urlGeral[code.value].QTDETOTAL} unidades deste produto.`
-                } else{
-                    info.innerHTML = `Não há informação sobre o produto ${urlGeral[code.value].PRODUTO}. `
+                    <p>Neste único lote, existem ${urlGeral[code.value].QTDE} até o dia ${urlGeral[code.value].VENC1}.</p>`
                 }
-            } )           
+
+                if(urlGeral[code.value].QTDE > 0 && urlGeral[code.value].QTDE2 > 0){
+                    info.innerHTML = `<p>Segue as informações do produto ${urlGeral[code.value].PRODUTO} abaixo:</p>
+                    <p><strong>Alerta!</strong> O formato de data está em: mês/dia/ano!
+                    <p>No primeiro lote, existem ${urlGeral[code.value].QTDE} até o dia ${urlGeral[code.value].VENC1}.</p>
+                    <p>No segundo lote, existem ${urlGeral[code.value].QTDE2} até o dia ${urlGeral[code.value].VENC2}.</p>
+                    <p>Totalizando um total de ${urlGeral[code.value].QTDETOTAL} unidades deste produto.`
+                }
+                
+                if(urlGeral[code.value].QTDE > 0 && urlGeral[code.value].QTDE2 > 0 && urlGeral[code.value].QTDE3 > 0){
+                    info.innerHTML = `<p>Segue as informações do produto ${urlGeral[code.value].PRODUTO} abaixo:</p>
+                    <p><strong>Alerta!</strong> O formato de data está em: mês/dia/ano!
+                    <p>No primeiro lote, existem ${urlGeral[code.value].QTDE} até o dia ${urlGeral[code.value].VENC1}.</p>
+                    <p>No segundo lote, existem ${urlGeral[code.value].QTDE2} até o dia ${urlGeral[code.value].VENC2}.</p>
+                    <p>No terceiro lote, existem ${urlGeral[code.value].QTDE3} até o dia ${urlGeral[code.value].VENC3}.</p>
+                    <p>Totalizando um total de ${urlGeral[code.value].QTDETOTAL} unidades deste produto.`
+                }
+
+                if(urlGeral[code.value].QTDE > 0 && urlGeral[code.value].QTDE2 > 0 && urlGeral[code.value].QTDE3 > 0 && urlGeral[code.value].QTDE4 > 0){
+                    info.innerHTML = `<p>Segue as informações do produto ${urlGeral[code.value].PRODUTO} abaixo:</p>
+                    <p><strong>Alerta!</strong> O formato de data está em: mês/dia/ano!
+                    <p>No primeiro lote, existem ${urlGeral[code.value].QTDE} até o dia ${urlGeral[code.value].VENC1}.</p>
+                    <p>No segundo lote, existem ${urlGeral[code.value].QTDE2} até o dia ${urlGeral[code.value].VENC2}.</p>
+                    <p>No terceiro lote, existem ${urlGeral[code.value].QTDE3} até o dia ${urlGeral[code.value].VENC3}.</p>
+                    <p>No quarto lote, existem ${urlGeral[code.value].QTDE4} até o dia ${urlGeral[code.value].VENC4}.</p>
+                    <p>Totalizando um total de ${urlGeral[code.value].QTDETOTAL} unidades deste produto.`
+                } 
+
+                if(urlGeral[code.value].QTDE > 0 && urlGeral[code.value].QTDE2 > 0 && urlGeral[code.value].QTDE3 > 0 && urlGeral[code.value].QTDE4 > 0 && urlGeral[code.value].QTDE5 > 0){
+                    info.innerHTML = `<p>Segue as informações do produto ${urlGeral[code.value].PRODUTO} abaixo:</p>
+                    <p><strong>Alerta!</strong> O formato de data está em: mês/dia/ano!
+                    <p>No primeiro lote, existem ${urlGeral[code.value].QTDE} até o dia ${urlGeral[code.value].VENC1}.</p>
+                    <p>No segundo lote, existem ${urlGeral[code.value].QTDE2} até o dia ${urlGeral[code.value].VENC2}.</p>
+                    <p>No terceiro lote, existem ${urlGeral[code.value].QTDE3} até o dia ${urlGeral[code.value].VENC3}.</p>
+                    <p>No quarto lote, existem ${urlGeral[code.value].QTDE4} até o dia ${urlGeral[code.value].VENC4}.</p>
+                    <p>No quinto lote, existem ${urlGeral[code.value].QTDE5} até o dia ${urlGeral[code.value].VENC5}.</p>
+                    <p>Totalizando um total de ${urlGeral[code.value].QTDETOTAL} unidades deste produto.`
+                }
+
+                if(urlGeral[code.value].QTDE === 0 && urlGeral[code.value].QTDE2 === 0 && urlGeral[code.value].QTDE3 === 0 && urlGeral[code.value].QTDE4 === 0 && urlGeral[code.value].QTDE5 === 0){
+                    info.innerHTML = `${urlGeral[code.value.PRODUTO]} não disponível no estoque`
+                }
+
+
+
+            })       
     } 
 }
